1a10732c29c785fec17586c4a97535a1b3cde552
Sashin Ramdhani <sashinramdhani@gmail.com>
2016-03-16 12:08:13 +0200

    Tutorial1-FizzBuzzWoof-part2

--- Tut-fizzbuzzwoof.cpp
+++ Tut-fizzbuzzwoof.cpp
@@ -3,14 +3,22 @@ using namespace std;
 int main()
 {
 	int i, a;
-	for (i = 1; i <= 20; i++)
+	for (i = 1; i <= 50; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0)
+			cout << "FizzBuzzWoof" << endl;
+		else if (i % 3 == 0 && i % 5 == 0)
 			cout << "FizzBuzz" << endl;
+		else if (i % 3 == 0 && i % 7 == 0)
+			cout << "FizzWoof" << endl;
+		else if (i % 5 == 0 && i % 7 == 0)
+			cout << "BuzzWoof" << endl;
 		else if (i % 3 == 0)
 			cout << "Fizz" << endl;
 		else if (i % 5 == 0)
 			cout << "Buzz" << endl;
+		else if (i % 7 == 0)
+			cout << "Woof" << endl;
 		else
 			cout << i << endl;
 	}
